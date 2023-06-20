60c1d9d957ad96523f619a2c75dd3d198a4fc800
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-06-20 20:51:00 -0300

    encadenando busquedas

--- myApp.js
+++ myApp.js
@@ -101,15 +101,21 @@ const removeById = (personId, done) => {
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
   Person.remove({ name: nameToRemove }, (err, data) => {
-    if(err) return console.log(err);
+    if (err) return console.log(err);
     done(null, data);
   })
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: { $all: [foodToSearch] } })
+    .sort({ name: 'asc' })
+    .limit(2)
+    .select('-age')
+    .exec((err, data) => {
+      if (err) return console.log();
+      done(null, data);
+    });
 };
 
 /** **Well Done !!**
